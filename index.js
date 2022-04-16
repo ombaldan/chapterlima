6c6ce0f7fb09174f20e03aec0d8baae6f85127d9
ombaldan <ombaldan@gmail.com>
2022-04-16 23:45:00 +0800

    Add file ejs & create form login

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
+const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
     res.render('index')
@@ -12,6 +14,19 @@ app.get('/', (req, res) => {
 app.get('/game', (req, res) => {
     res.render('game')
     console.log("ini halaman game")
+})
+
+app.get('/login', (req, res) => {
+    res.render('login')
+    console.log('Halaman login')
+  })
+
+app.post('/login', (req, res) => {
+  //  Login Code 
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`)
+  console.log('Login sukses')
 })
 
 app.listen(2000, () => {
